9d23a520381ff503bcfc09f2fbc59b07c934e975
libbyschuknight <libbysk@gmail.com>
2015-09-16 21:31:29 +1200

    Add create a new employee API endpoint

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -5,27 +5,27 @@ module Employee
       desc "List all Employee"
 
       get do
-        # the tutorial had EmpData.all below when I created app but the model that was made was emp_datum.rb. So changed below and works.
+        # the tutorial had EmpData.all below when I created app but the model
+        # that was made was emp_datum.rb. So changed below and works.
 
         EmpDatum.all
       end
+
+      desc "create a new employee"
+      ## This takes care of parameter validation
+      params do
+        requires :name, type: String
+        requires :address, type:String
+        requires :age, type:Integer
+      end
+      ## This takes care of creating employee
+      post do
+        EmpDatum.create!({
+          name:params[:name],
+          address:params[:address],
+          age:params[:age]
+        })
+      end
     end
-
-    # desc "create a new employee"
-    # ## This takes care of parameter validation
-    # params do
-    #   requires :name, type: String
-    #   requires :address, type:String
-    #   requires :age, type:Integer
-    # end
-
-    # ## This takes care of creating employee
-    # post do
-    #   EmpData.create!({
-    #     name:params[:name],
-    #     address:params[:address],
-    #     age:params[:age]
-    #   })
-    # end
   end
 end
