2ee48d952a48717224fd9c0aa4e50dc3c4403c3e
libbyschuknight <libbysk@gmail.com>
2015-09-17 09:26:36 +1200

    Add delete and update API endpoints

--- README.md
+++ README.md
@@ -3,30 +3,29 @@
 App made from following "Build Great APIS with Grape
 " tutorial http://www.sitepoint.com/build-great-apis-grape/
 
-Syntax for doing post using curl
+Syntax for doing post using curl:
 
 ```
- curl -d "name=bob" -d "address=mumbai" -d "age=33" http://localhost:3000/api/v1/employee_data.json
+curl -d "name=bob" -d "address=mumbai" -d "age=33" http://localhost:3000/api/v1/employee_data.json
 ```
 
-instead of
+instead of:
 
 ```
- curl http://localhost:3000/api/v1/employee_data.json -d "name=jay;address=delhi;age=25"
+curl http://localhost:3000/api/v1/employee_data.json -d "name=jay;address=delhi;age=25"
 ```
-which was from tutorial but didn't work for me. Kept on getting this error
+which was from tutorial but didn't work for me. Kept on getting this error:
 ```
 {"error":"address is missing, age is missing"}
 ```
 
-
-This worked as well
+This worked as well:
 
 ```
- curl -XPOST -H "Content-Type: application/json" "localhost:3000/api/v1/employee_data" -d '
- {
-   "name": "Kate",
-   "address": "London",
-   "age": "42"
-   }'
+curl -XPOST -H "Content-Type: application/json" "localhost:3000/api/v1/employee_data" -d '
+{
+  "name": "Kate",
+  "address": "London",
+  "age": "42"
+  }'
 ```
--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -13,19 +13,42 @@ module Employee
 
       desc "create a new employee"
       ## This takes care of parameter validation
-      params do
-        requires :name, type: String
-        requires :address, type:String
-        requires :age, type:Integer
-      end
-      ## This takes care of creating employee
-      post do
-        EmpDatum.create!({
-          name:params[:name],
-          address:params[:address],
-          age:params[:age]
-        })
-      end
+        params do
+          requires :name, type: String
+          requires :address, type:String
+          requires :age, type:Integer
+        end
+
+        ## This takes care of creating employee
+        post do
+          EmpDatum.create!({
+            name:params[:name],
+            address:params[:address],
+            age:params[:age]
+          })
+        end
+
+      desc "delete an employee"
+        params do
+          requires :id, type: String
+        end
+
+        delete ':id' do
+          EmpDatum.find(params[:id]).destroy!
+        end
+
+      desc "update an employee address"
+        params do
+          requires :id, type: String
+          requires :address, type:String
+        end
+
+        put ':id' do
+          EmpDatum.find(params[:id]).update({
+            address:params[:address]
+          })
+        end
+
     end
   end
 end
