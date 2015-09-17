1bf2901c15f5ed80bdaf61b73f009b0dde1ab1ff
libbyschuknight <libbysk@gmail.com>
2015-09-17 12:06:57 +1200

    Add grape-swagger-rails to get documentation for API
    Have been able to get the Swagger documentation page up but cannot get any API documentation showing

--- Gemfile
+++ Gemfile
@@ -44,3 +44,7 @@ group :development, :test do
 end
 
 gem 'grape'
+gem 'grape-entity'
+gem 'rack-contrib'
+gem 'grape-swagger'
+gem 'grape-swagger-rails'
--- app/api/api.rb
+++ app/api/api.rb
@@ -2,4 +2,6 @@ class API < Grape::API
   prefix 'api'
   version 'v1', using: :path
   mount Employee::Data
+
+  add_swagger_documentation api_version: 'api'
 end
